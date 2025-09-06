1adc8d42599537cbb24b4f2a592e465b92368e46
Moses KracarTee Norris <moseskracartee@hotmail.com>
2025-09-06 09:02:38 +0000

    done with exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,79 @@
 using System;
 
-class Program
+public class GradeCalculator
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int percent))
+        {
+            if (percent >= 0 && percent <= 100)
+            {
+                string letterGrade = GetLetterGrade(percent);
+                Console.WriteLine($"Your letter grade is: {letterGrade}");
+                if (percent >= 70)
+            {
+                Console.WriteLine("Congratulations! You passed!");
+            }
+            else
+            {
+                Console.WriteLine("We are sorry! Good luck next time!");
+            }
+            }
+            else
+            {
+                Console.WriteLine("Score must be between 0 and 100.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a numerical value.");
+        }
+    }
+
+    public static string GetLetterGrade(int percent)
+    {
+        string grade = "";
+        string sign = "";
+
+        // Determine the letter grade
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent >= 80)
+        {
+            grade = "B";
+        }
+        else if (percent >= 70)
+        {
+            grade = "C";
+        }
+        else if (percent >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+        
+        // Determine the plus/minus sign for grades A, B, C, D
+        if (grade != "F")
+        {
+            int lastDigit = percent % 10;
+            if (lastDigit >= 7 || (grade == "A" && percent == 100)) // A+ for 100
+            {
+                sign = "+";
+            }
+            else if (lastDigit <= 2)
+            {
+                sign = "-";
+            }
+        }
+
+        return grade + sign;
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,33 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+   static void Main(string[] args)
+        {
+        Random randomGenerator = new Random();
+
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+       {
+           Console.WriteLine("What is your guess number? ");
+           guess = int.Parse(Console.ReadLine());
+           if (magicNumber > guess)
+           {
+               Console.WriteLine("Higher");
+           }
+           else if (magicNumber < guess)
+           {
+                Console.WriteLine("Lower");
+           }
+           else
+           {
+              Console.WriteLine("You guessed it!");
+           }
+
+        } 
     }
+
+
 }
