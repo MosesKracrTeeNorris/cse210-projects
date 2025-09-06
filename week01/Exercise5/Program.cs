1d806384d405314cef91dba14ae12c8082bb2e9d
Moses KracarTee Norris <moseskracartee@hotmail.com>
2025-09-06 20:36:23 +0000

    Function vs Methods

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,31 @@
 using System;
-
-class Program
+class program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
-    }
+   static void Main(string[] args)
+   {
+    DisplayWelcomeMessage();
+
+    string userName = PromptUserName();
+    int squaredNumber = squaredNumber(userNumber);
+    DisplayResult(userName, squareNumber);
+
+   }
+   static void DisplayWelcomeMessage()
+   {
+    Console.WriteLine("Please enter your name:");
+    string name = Console.ReadLine();
+
+    return name;
+   }
+   static int SquareNumber(int number)
+   {
+    int square = number * number;
+    return square;
+   }
+   static void DisplayResult(string name, int square)
+   {
+
+       Console.WriteLine($"{name}, the square of your number is {square}");
+   }
+
 }
