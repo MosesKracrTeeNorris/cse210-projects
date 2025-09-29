ce902344658e3951e111e4b937f9556b63f2bfff
Moses KracarTee Norris <moseskracartee@hotmail.com>
2025-09-29 08:46:13 +0000

    Homework files added

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -1,20 +1,20 @@
-using System;
+//using System;
 
 public class Entry
 {
     public string _date;
     public string _promptText;
     public string _entryText;
-    public string _mental;
-    public string _time; 
+    
+    //public string _time; 
 
     public void Display()
     {
-        Console.WriteLine($"Date: {_date}");
-        Console.WriteLine($"Time: {_time}");
-        Console.WriteLine($"Prompt: {_promptText}");
-        Console.WriteLine($"Entry: {_entryText}");
-        Console.WriteLine($"Mental: {_mental}");
+        //Console.WriteLine($"Date: {_date}");
+        //Console.WriteLine($"Time: {_time}");
+       // Console.WriteLine($"Prompt: {_promptText}");
+       // Console.WriteLine($"Entry: {_entryText}");
+       // Console.WriteLine($"Mental: {_mental}");
             
     }
 }
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Fenda Bondo", "Subtraction");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Nancy Boakai", "Division", "25.5", "15/5");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("Modaris D.B. Norris", "Church History", "The purpose of Mormon Army");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
