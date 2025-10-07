0441daa50ebf1ecb9ae365f9d9f01ea0994c7f7a
Moses KracarTee Norris <moseskracartee@hotmail.com>
2025-10-07 03:46:51 +0000

    Added shape files

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        Square s1 = new Square("Orange", 4);
+        shapes.add(s1);
+
+        Rectangle s2 = new Rectangle("Blue", 5, 6);
+        shapes.add(s2);
+
+        Circle s3 = new Circle("Gray", 7);
+        shapes.add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
